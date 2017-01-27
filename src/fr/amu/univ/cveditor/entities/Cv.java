0ad38431c1d35e0e0f668a01b68bede95f14d651
Benjamin <benj.magron@gmail.com>
2017-01-27 11:42:51 +0100

    Delete ConnectedUser

--- src/fr/amu/univ/cveditor/entities/Cv.java
+++ src/fr/amu/univ/cveditor/entities/Cv.java
@@ -3,6 +3,7 @@ package fr.amu.univ.cveditor.entities;
 import java.util.ArrayList;
 
 import javax.persistence.ElementCollection;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
@@ -12,6 +13,7 @@ public class Cv {
 	@Id
 	public int id;
 	
+	@Embedded
 	@ElementCollection
 	public ArrayList<Activite> activites;
 	
